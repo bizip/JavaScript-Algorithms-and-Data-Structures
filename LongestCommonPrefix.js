ddc5b1aaf73e78f331c91304c551cda2e147899e
Bizimungu Pascal <bizip@DESKTOP-TA09OM0.localdomain>
2023-01-23 09:46:19 +0200

    Fix longestCommonPrefix challenge

--- LongestCommonPrefix.js
+++ LongestCommonPrefix.js
@@ -1,14 +1,19 @@
 let strs = ["flower", "flow", "flight"];
-var longestCommonPrefix = function (arr) {
-    if(strs.length === 0) return "";
-    let prefix =arr[0];
-    for(let i=1;i<arr.length;i++){
-      while(arr[i].indexOf(prefix) !== 0){
-        prefix =prefix.substring(0,arr.length -1);
-        if(prefix === '') return "";
+
+// 1. initialize the firest element in array as common prefix
+// 2. loop throught the array
+// 3. while all array's element are part of prefix and assign it to prefix
+// 4. if prefix = "" return ""
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) return "";
+      let prefix = strs[0];
+      for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+          prefix = prefix.substring(0, prefix.length - 1);
+          if (prefix === "") return "";
+        }
       }
-    }
-console.log(prefix);
-};
+      return prefix;
+    };
 longestCommonPrefix(strs)
 
