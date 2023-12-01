6c0d0a74bd883eb13d915258a07fb9a25a270d40
bizip <bizip04@gmail.com>
2023-12-01 22:52:19 +0200

    Add new solution

--- AddUpToN.js
+++ AddUpToN.js
@@ -1,3 +1,7 @@
-const AddUppToN=(n)=>{
-    return n*(n+1)/2
+// Write a JavaScript function that calculates the sum of all 
+// numbers from 1 up to (including) a given number, n.
+const AddUppToN = (n) =>{
+return n * (n+1)/2;
 }
+
+console.log(AddUppToN(6));
