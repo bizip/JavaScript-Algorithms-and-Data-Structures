b5ef92130a2aef66ee63c4082d59d322e358e824
Bizimungu Pascal <bizip04@gmail.com>
2025-01-03 23:36:39 +0200

    Add the firest version with nested loop

--- sameValue.js
+++ sameValue.js
@@ -1,4 +1,11 @@
 const same = (arr1, arr2) =>{
   if (arr1.length !== arr2.length) return;
-
+  for(let i =0; i <  arr1.length; i ++){
+    if(!arr2.includes((arr1[i]**2))) return console.log(false);
+    
+  }
+  console.log(true);
+  return true;
 }
+
+same([1,2,3], [4,1,12]);
