3fb0b1170809861fe9656800ff074054b41bf1a2
Bizimungu Pascal <bizip04@gmail.com>
2025-01-04 00:02:46 +0200

    Sart 0 of n solution

--- sameValue.js
+++ sameValue.js
@@ -1,11 +1,22 @@
+// const same = (arr1, arr2) =>{
+//   if (arr1.length !== arr2.length) return;
+//   for(let i =0; i <  arr1.length; i ++){
+//     if(!arr2.includes((arr1[i]**2))) return console.log(false);
+    
+//   }
+//   console.log(true);
+//   return true;
+// }
+
+// same([1,2,3], [4,1,9]);
+
 const same = (arr1, arr2) =>{
   if (arr1.length !== arr2.length) return;
-  for(let i =0; i <  arr1.length; i ++){
-    if(!arr2.includes((arr1[i]**2))) return console.log(false);
-    
-  }
-  console.log(true);
-  return true;
+ let obj1 ={};
+ for(let i =0; i <  arr1.length; i ++){
+  // obj1[arr1[i]] ? obj1[arr1[i]] +1 : ;
+ }
+ console.log(obj1);
 }
 
 same([1,2,3], [4,1,12]);
