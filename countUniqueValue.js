b8be5fb1e950badb296bca7d838104b583c43e8f
Bizimungu Pascal <bizip04@gmail.com>
2025-01-05 22:40:14 +0200

    Understand how dynamic programming work with example

--- countUniqueValue.js
+++ countUniqueValue.js
@@ -1,13 +1,4 @@
 const countUniqueValue = (arr) =>{
-    let counterValue = 0;
-    const counter = {};
-    for(let val of arr){
-        counter[val] = (counter[val] || 0) + 1
-    }
-    for(let val in counter){
-        counterValue += (counter[val]);
-    }
-console.log(counterValue);
-return counterValue;
+   
 }
 countUniqueValue([1,1,1,1,1,2]);
