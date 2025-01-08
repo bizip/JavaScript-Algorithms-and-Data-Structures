6a7c444159f991648373f1acece7d2ea052fe612
Bizimungu Pascal <bizip04@gmail.com>
2025-01-08 23:41:56 +0200

    Use o of n solution

--- countUniqueValue.js
+++ countUniqueValue.js
@@ -1,5 +1,12 @@
 const countUniqueValue = (arr) =>{
-    console.log(arr);
+    if(arr.length === 0) return 0;
+    let counter = 0;
+    const newArr = new set(arr);
+    console.log(newArr);
+    // for(let val of arr){
+    //     console.log(val);
+    // }
+
    
 }
 countUniqueValue([1,1,1,1,1,2]);
